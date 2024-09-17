9f4a12863f1f08c87c6be6308191377c07541a4d
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-09-17 06:33:21 +0100

    :sparkles: (Movie) add movie serializer

---
+++ tests/movies/test_serializer.py
@@ -0,0 +1,14 @@
+from movies.serializers import MovieSerializer
+
+def test_valid_movie_serializer():
+    valid_serializer_data = {
+        "title": "Raising Arizona",
+        "genre": "comedy",
+        "year": "1987"
+    }
+
+    serializer = MovieSerializer(data=valid_serializer_data)
+    assert serializer.is_valid()
+    assert serializer.validated_data == valid_serializer_data
+    assert serializer.data == valid_serializer_data
+    assert serializer.errors == {}
