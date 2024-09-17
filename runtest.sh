26ac24333240a244cd557b25a0d96c7680216e8f
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-09-17 08:05:23 +0100

    :construction_worker: (CI) add test cov

--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker compose exec -e DJANGO_TEST=true movies poetry run pytest --reuse-db
+docker compose exec -e DJANGO_TEST=true movies poetry run pytest -p no:warnings --cov=. --reuse-db
 
 
 
